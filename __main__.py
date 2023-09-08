ae1e40c4284b6e631e11cf5cdcc30a656381a34d
roundspecs <2502.zarif@gmail.com>
2023-09-08 08:15:23 +0600

    change to floor

--- __main__.py
+++ __main__.py
@@ -2,10 +2,10 @@ import codeforces_api
 from datetime import datetime
 
 class User:
-  def __init__(self,handle,solve_count,rounded_rating) -> None:
+  def __init__(self,handle,solve_count,floored_rating) -> None:
     self.handle = handle
     self.solve_count = solve_count
-    self.rounded_rating = rounded_rating
+    self.floored_rating = floored_rating
   def __lt__(self, other):
     return self.solve_count > other.solve_count
   def __str__(self):
@@ -17,9 +17,9 @@ unix_today = datetime.combine(datetime.today().date(), datetime.min.time()).time
 
 users = [User(handle,0,0) for handle in input("Handles (space separated): ").split(' ')]
 for i,info in enumerate(cf_api.user_info(handles=[user.handle for user in users])):
-  users[i].rounded_rating = round(info.max_rating/100)*100
+  users[i].floored_rating = (info.max_rating//100)*100
 
-print("\nGood problems = {problem | problem.rating >= round(user.max_rating)}\n")
+print("\nGood problems = {problem | problem.rating >= floor(user.max_rating)}\n")
 print("Good problems solved:")
 for user in users:
   print(user.handle, end=": ")
@@ -31,7 +31,7 @@ for user in users:
     if submission.verdict != "OK":
       continue
     problem: codeforces_api.Problem = submission.problem
-    if problem.rating < user.rounded_rating:
+    if problem.rating==None or problem.rating < user.floored_rating:
       continue
     acs.add(f"{problem.contest_id}{problem.index}")
   print(','.join(acs))
